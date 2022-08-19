98d094095ef83293f7858381836d088177ee8e8d
Alpimax <alperrozkann@gmail.com>
2022-08-19 11:48:28 -0400

    Added variable AccessModifiers.java files

---
+++ src/My_utils/TestingAccessOutside.java
@@ -0,0 +1,13 @@
+package My_utils;
+
+import Day38_AccessModifiers.AccessModifiers;
+
+public class TestingAccessOutside {
+    public static void main(String[] args) {
+        AccessModifiers obj = new AccessModifiers();
+        System.out.println(obj.a);
+        //        System.out.println(obj.b); b is default, which is only visible in the same package, so it cannot be accessed in a different package
+//        System.out.println(obj.c); c is private, so its only visible in the same class
+
+    }
+}
