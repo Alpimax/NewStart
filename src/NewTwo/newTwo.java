5bbeef46d122cfeb98b4729f1f8f24f735c41180
Alpimax <alperrozkann@gmail.com>
2022-08-19 09:21:28 -0400

    new stuff

--- src/NewTwo/newTwo.java
+++ src/NewTwo/newTwo.java
@@ -5,6 +5,7 @@ public class newTwo {
 
     public static void main(String[] args) {
         System.out.print("Alper");
-
+        System.out.println();
+        System.out.println("alper");
     }
 }
