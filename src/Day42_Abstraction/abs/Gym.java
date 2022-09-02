93475edca4109a25bea535946d109f6977f82893
Alpimax <alperrozkann@gmail.com>
2022-09-02 15:18:06 -0400

    OutOfStockException created

---
+++ src/Day42_Abstraction/abs/Gym.java
@@ -0,0 +1,13 @@
+package Day42_Abstraction.abs;
+
+public class Gym {
+    public static void main(String[] args) {
+
+//        Exercise obj = new Exercise(); not valid because Exercise is abstract - cannot create objects from it
+
+        Running run = new Running();
+        run.perform();
+        run.getCaloriesBurned(30);
+
+    }
+}
