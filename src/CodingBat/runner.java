19725100d53f605860b8b53205d43c15105fbe8a
Alpimax <alperrozkann@gmail.com>
2022-09-05 21:08:25 -0400

    CodingBat

---
+++ src/CodingBat/runner.java
@@ -0,0 +1,13 @@
+package CodingBat;
+
+public class runner {
+    public static void main(String[] args) {
+
+
+        CodingBat myCoding = new CodingBat(5,"yellow","b");
+
+        System.out.println(myCoding);
+
+    }
+
+}
