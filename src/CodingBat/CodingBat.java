9efaa29cb32fffff920b95d4da69725756f8a403
Alpimax <alperrozkann@gmail.com>
2022-09-07 18:19:53 -0400

    Armstrong  created.

--- src/CodingBat/CodingBat.java
+++ src/CodingBat/CodingBat.java
@@ -5,24 +5,24 @@ public class CodingBat {
     public static void main(String[] args) {
 
 
-    int number =6232416;
-    int count = 0;
-    int total = 0;
-    int value;
-    int temp = number;
+        int number = 6232416;
+        int count = 0;
+        int total = 0;
+        int value;
+        int temp = number;
 
-    while( temp>0){
-        temp/=10;
-        count++;
-    }
+        while (temp > 0) {
+            temp /= 10;
+            count++;
+        }
         System.out.println(count);
         System.out.println(temp);
         temp = number;
-    while(temp>0){
-    value = temp % 10;
-    total += Math.pow(value,count);
-    temp /= 10;
-    }
+        while (temp > 0) {
+            value = temp % 10;
+            total += Math.pow(value, count);
+            temp /= 10;
+        }
         System.out.println(number == total ? "this is Armstrong. " : "this is not Armstrong.");
     }
 
