4ea3122df0fd7c27937f0a42fef098d583e44ee2
Alpimax <alperrozkann@gmail.com>
2022-09-07 23:47:56 -0400

    Armstrong  created.

---
+++ src/CodingBat/LetsTry.java
@@ -0,0 +1,27 @@
+package CodingBat;
+
+public class LetsTry {
+    public static void main(String[] args) {
+        int number = 2004;
+
+        if (number % 400 == 0 && number % 4 == 0 ){
+            System.out.println(true);
+        }else
+            System.out.println(false);
+int n= 0;
+String b ="";
+
+
+        }
+
+        CodingBat obj = new CodingBat();
+
+    public static int click (int n, String b){
+
+
+        return n;
+    }
+
+
+}
+
