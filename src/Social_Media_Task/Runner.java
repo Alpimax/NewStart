8eb126a8197b32d822e4c3572296ee83ea119b6a
Alpimax <alperrozkann@gmail.com>
2022-09-17 10:55:17 -0400

    Has Picture  class created.

--- src/Social_Media_Task/Runner.java
+++ src/Social_Media_Task/Runner.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Runner {
     public static void main(String[] args) {
-        Facebook obj = new Facebook("Megan Tice","Megan");
+        Facebook obj = new Facebook("Megan Tice", "Megan");
         obj.notifications();
         System.out.println(obj);
         System.out.println(obj);
