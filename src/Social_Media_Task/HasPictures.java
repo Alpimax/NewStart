728549512faf162231d68bc4676e0e3505b58e6b
Alpimax <alperrozkann@gmail.com>
2022-09-17 10:54:50 -0400

    Has Picture  class created.

--- src/Social_Media_Task/HasPictures.java
+++ src/Social_Media_Task/HasPictures.java
@@ -3,9 +3,11 @@ package Social_Media_Task;
 public interface HasPictures {
 
 
-void likePicture();
-void unLikePicture();
-void sharePicture();
+    void likePicture();
+
+    void unLikePicture();
+
+    void sharePicture();
 
 
 //• Implement this interface to the Instagram class. (The actual implementation does not matter. Print statement related to action. Focus on idea of how abstraction is working)
