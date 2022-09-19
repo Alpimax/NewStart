50e17d668fe46d6bd18e7c086688df9799e5213e
Alpimax <alperrozkann@gmail.com>
2022-09-19 16:50:00 -0400

    Has Picture  class cr eated.

--- src/list/ListObjects.java
+++ src/list/ListObjects.java
@@ -29,14 +29,32 @@ public class ListObjects {
         list3.add('c');
         System.out.println(list3);
 
+                Set<String> set1 = new HashSet<>(); // random order
+                set1.add(null);
+                set1.add("hello world");
+                set1.add("5.0");
+                set1.add("#zebra");
+                set1.add("hello world");
+                set1.add("Java");
+                System.out.println(set1);
 
-        Set<String> set1 = new HashSet<>();
-        set1.add(null);
-        set1.add("hello world");
-        set1.add("5.0");
-        set1.add("#zebra");
-        set1.add("hello world");
-        set1.add("Java");
-        System.out.println(set1);
-    }
-}
+                Set<String> set2 = new LinkedHashSet<>(); // maintains insertion order
+                set2.add(null);
+                set2.add("hello world");
+                set2.add("5.0");
+                set2.add("#zebra");
+                set2.add("hello world");
+                set2.add("Java");
+                System.out.println(set2);
+
+                Set<String> set3 = new TreeSet<>(); // maintains natural order
+                // set3.add(null); // null is not allowed in TreeSet
+                set3.add("hello world");
+                set3.add("5.0");
+                set3.add("#zebra");
+                set3.add("hello world");
+                set3.add("Java");
+                System.out.println(set3);
+
+            }
+        }
