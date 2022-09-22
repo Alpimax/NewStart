5c829132567ca3a58ddd7f0ae19c9103abee642a
Alpimax <alperrozkann@gmail.com>
2022-09-21 22:51:55 -0400

    .

--- src/list/HomeWork.java
+++ src/list/HomeWork.java
@@ -27,8 +27,6 @@ public class HomeWork {
 
     }
 
-
-
     public static int divideOperator(int number, int num) {
         //   2) Numbers - Divide without / operator
 //Write a method that can divide two numbers without using division operator
